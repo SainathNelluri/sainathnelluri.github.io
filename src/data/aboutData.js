71c25ae0b3dcec55589fd3b458467150232abe65
Sainath Nelluri <sainathnelluri@Sainaths-MacBook-Pro.local>
2024-06-11 22:39:28 -0500

    about updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Sainath Nelluri I'm a DevOps Engineer based in Tx.",
+    description1: "My name's Sainath Nelluri I'm a DevOps Engineer based in Texas.",
     description2: "With three years of hands-on IT experience, I specialize in DevOps methodologies, showcasing proficiency in managing cloud infrastructure, implementing CI/CD pipelines, and automating processes using Terraform, Docker, and Kubernetes. I have a proven track record of enhancing operational efficiency, reducing deployment times, and improving code quality.",
     image: 1
 }
