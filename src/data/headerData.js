f02039123b569d06bfb8eeb697b94428d56a391a
SainathNelluri <163747721+SainathNelluri@users.noreply.github.com>
2024-06-11 22:12:22 -0500

    Updated headerinfo.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,8 @@ import image from '../assets/images/profile.jpg'
 export const headerData = {
     name: 'Sainath Nelluri',
     title: "Devops Engineer",
-    desciption:"Master's graduate in Computer and Information Science, showcasing top academic performance and nearly 3 years of practical IT experience. Proficient in Java, Spring Boot, and DevOps methodologies, with a dedicated focus on continuous integration/continuous deployment (CI/CD) pipelines. Experienced in implementing cloud and hybrid solutions on Microsoft Azure and adept at collaborating with cross-functional teams to ensure seamless deployment and operation of applications",
+    desciption:"Master's graduate in Computer and Information Science, showcasing top academic performance and nearly 3 years of practical IT experience. Proficient in DevOps methodologies, with a dedicated focus on continuous integration/continuous deployment (CI/CD) pipelines. Experienced in implementing cloud and hybrid solutions on Microsoft Azure.", 
+        //and adept at collaborating with cross-functional teams to ensure seamless deployment and operation of applications
     image: image,
     resumePdf: resume
 }
